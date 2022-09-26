89dcf9cc285617c7bfaf7b6385ebedd0b1814a01
AbbasSayed <eng.abbas@hotmail.com>
2022-09-26 08:03:58 +0200

    DomainLayer

---
+++ DnBooking/DnBooking.Domain/Valiations/HotelValidation.cs
@@ -0,0 +1,17 @@
+﻿using DnBooking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnBooking.Domain.Valiations
+{
+    public class HotelValidation
+    {
+        public bool IsHotelValid(Hotel hotel)
+        {
+            return true;
+        }
+    }
+}
