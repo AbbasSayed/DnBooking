49fabb3655c0480b918ad4620f1c9b94ce593c66
AbbasSayed <eng.abbas@hotmail.com>
2022-09-26 16:40:20 +0200

    SetupVersioning

---
+++ DnBooking/DnBooking.Api/Controllers/V2/HotelController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DnBooking.Api.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HotelController : ControllerBase
+    {
+        public HotelController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHotels()
+        {
+            return Ok("Hello from Hotel Controller v2 !");
+        }
+    }
+}
---
+++ DnBooking/DnBooking.Api/Options/ConfigureSwaggerOptions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace DnBooking.Api.Options
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+            }
+
+           
+
+        }
+
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo
+            {
+                Title = "DNSocial",
+                Version = description.ApiVersion.ToString(),
+                Description = "Through this API you can reserve a room in a hotel",
+                Contact = new()
+                {
+                    Email = "eng.abbas@hotmail.com",
+                    Name = "Abbas Sayed",
+                },
+                License = new()
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description = "This API is deprecated";
+            }
+
+            return info;
+        }
+    }
+}
